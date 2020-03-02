5d767a6c12ed2d8e55d76515ee70e1e5017ec5c0
JCBoyd <jcboyd@pm.me>
2020-03-02 13:37:53 -0700

    Create project shell, commit, and push to remote

---
+++ src/circleclassproject/CircleClassProject.java
@@ -0,0 +1,21 @@
+package circleclassproject;
+
+
+
+public class CircleClassProject {
+
+/******************************************************************************
+ * CircleClassProject.java 
+ * Programmer: @author jcboyd
+ * Version: 1.0
+ * Course: SDEV 2210
+ *
+ * This program calculates the diameter, circumference, and area of a circle
+ * based on user input of the radius
+ *****************************************************************************/
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
